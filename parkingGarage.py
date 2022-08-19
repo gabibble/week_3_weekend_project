dda5b7c4e4ca1a3eeccfd5966f3359f6a03ae471
gabibble <juliagieb@gmail.com>
2022-08-19 09:24:19 -0700

    made classes

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,1 +1,37 @@
-# Start Your Code here
+class Customer():
+    def __init__(self):
+        self.name = "name"
+        self.customerInfo = {}
+
+    def Park(self):
+        response = input("What's your name?").lower()
+        self.customerInfo['name'] = response
+        self.customerInfo['tckNumb'] = 1
+        self.customerInfo['parkingSpace'] = 1
+
+
+
+class ParkingGarage():
+    def __init__(self):
+        self.tickets = []
+        self.parkingSpaces = list(range(20))
+        self.currentTickets = {}
+
+    def takeTicket(self):
+        if len(self.parkingSpaces) > 0:
+            print("TICKT TAKEN")
+            print(self.parkingSpaces)
+        else:
+            print("NO TIX")
+
+
+    def payForParking(self):
+        pass
+
+    def leaveGarage(self):
+        pass
+
+
+park = ParkingGarage()
+
+park.takeTicket()
